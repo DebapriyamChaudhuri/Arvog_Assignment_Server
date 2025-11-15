92e6eb14b2f3745340b7a40580213751bfb4cbee
Debapriyam Chaudhuri <debapriyam2016@gmail.com>
2025-11-16 03:43:56 +0530

    setup done

--- src/server.js
+++ src/server.js
@@ -1,19 +1,10 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-// Base route
 app.get("/", (req, res) => {
-  res.json({ message: "Arvog Assignment Server is running 🚀" });
+  res.send("Hello from Node.js server!");
 });
 
-// Import routes
-app.use("/api/users", require("./routes/user.routes"));
-app.use("/api/categories", require("./routes/category.routes"));
-app.use("/api/products", require("./routes/product.routes"));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
